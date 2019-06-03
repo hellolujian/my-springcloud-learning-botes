d8ce6098da30cb02cbd43412662a3312f4e88ff3
lujian01 <lujian01@hujiang.com>
2019-06-03 20:25:00 +0800

    update

--- soa-group/src/main/java/com/example/soagroup/controller/TestController.java
+++ soa-group/src/main/java/com/example/soagroup/controller/TestController.java
@@ -2,6 +2,7 @@ package com.example.soagroup.controller;
 
 import com.example.soagroup.model.Group;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/group/v1.1/test")
+@RefreshScope
 public class TestController {
 
     @Value("${configInfo}")
