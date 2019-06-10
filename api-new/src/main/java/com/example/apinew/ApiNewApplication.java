9cf9e13c443eeebec85a14678992b8d3ca869b0b
lujian01 <lujian01@hujiang.com>
2019-06-10 13:19:21 +0800

    update

--- api-new/src/main/java/com/example/apinew/ApiNewApplication.java
+++ api-new/src/main/java/com/example/apinew/ApiNewApplication.java
@@ -2,11 +2,9 @@ package com.example.apinew;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
